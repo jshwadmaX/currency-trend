91d28656c3ab6da448e45f0547d849a23b618c5d
Arya Shinde <aryabshinde@gmail.com>
2025-12-16 11:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 # MySQL configuration
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "root123"
-app.config["MYSQL_DB"] = "currency_trend_management"
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
 
 # Initialize MySQL
 mysql = MySQL(app)
@@ -578,3 +579,4 @@ def edit_profile():
 # Run the app
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
+
